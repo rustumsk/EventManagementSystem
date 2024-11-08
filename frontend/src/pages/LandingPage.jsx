ddd58dca4f6f64b0675a153e4e658f539183e90c
Rustum Arellano <rustumarellano@gmail.com>
2024-11-08 23:47:37 +0800

    making landing page not complete note even a design

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,19 @@
+import '../styles/landing.scss';
+
+export default function LandingPage(){
+    return(
+        <div className="l-container">
+          <section className="sec first">
+            <header className='f-header'>Hello</header>
+            <section className='f-body'>
+              <section></section>
+              <section></section>
+            </section>
+          </section>
+          <section className="sec second"></section>
+          <section className="sec third"></section>
+          <section className="sec fourth"></section>
+          <section className="sec fifth"></section>
+        </div>
+    )
+}
